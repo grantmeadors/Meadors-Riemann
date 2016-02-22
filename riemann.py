a7dd20bac174180c39266fd1f867d51e46ae0538
Grant David Meadors <grant.meadors@ligo.org>
2016-02-22 21:53:35 +0100

    Experiments with the Riemann tensor

---
+++ riemann.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# Grant David Meadors
+# 02016-02-22 (JD 2457441)
+# g r a n t . m e a d o r s @ a e i . m p g . d e
+# Experimenting with the Riemann tensor
+print 'Hello'
+
+
